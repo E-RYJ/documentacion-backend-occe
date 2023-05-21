a73aff76db2f24fa0005c4ec944d4f6ae62e8160
E-RYJ <77464890+E-RYJ@users.noreply.github.com>
2023-05-21 12:49:25 -0700

    Update SpringdocConfig.java

--- src/main/java/com/occe/SpringdocConfig.java
+++ src/main/java/com/occe/SpringdocConfig.java
@@ -12,7 +12,7 @@ import io.swagger.v3.oas.annotations.info.License;
 @OpenAPIDefinition(
     info = @io.swagger.v3.oas.annotations.info.Info(
         title = "API REST \"OCCE\"",
-        description = "Esta es una API REST para realizar operaciones en el sistema de ORIENTACION CURRICULAR PARA CORDINADORES Y ESTUDIANTES\n \"OCCE\"",
+        description = "Esta es una API REST para realizar operaciones en el sistema de ORIENTACION CURRICULAR PARA CORDINADORES Y ESTUDIANTES \"OCCE\". El propósito principal de esta API REST desarrollada con Spring es proporcionar una interfaz de programación de aplicaciones (API) basada en los principios y estándares de REST (Representational State Transfer). La API REST de Spring permite la comunicación y transferencia de datos entre diferentes aplicaciones y sistemas de manera eficiente y escalable. Al utilizar Spring Boot, se simplifica el proceso de desarrollo de la API, ya que proporciona un marco de trabajo sólido y herramientas integradas para crear endpoints HTTP",
         version = "1.0.0",
         license = @License(
             name = "Licencia Apache 2.0",
